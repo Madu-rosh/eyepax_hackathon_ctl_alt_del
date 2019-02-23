cc7c571920fa112b1465a17dea1ba0115792b977
roshanKaru <roshan@healthepro.com>
2019-02-23 19:17:37 +0530

    angular-4 changes addeed route module

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from "./app.component";
+import { HomeComponent } from './home/home.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { PostComponentComponent } from './post-component/post-component.component';
+import { CommentComponentComponent } from './comment-component/comment-component.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, HomeComponent, LoginComponentComponent, PostComponentComponent, CommentComponentComponent],
+  imports: [BrowserModule, AppRoutingModule,HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
